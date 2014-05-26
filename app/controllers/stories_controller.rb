eff21ac492b3dca47a3f90e613a6562424afbcb0
benoitlecorre <bnt.lecorre@gmail.com>
2014-05-26 17:17:42 +0200

    drop the databse and destroy deivse user to create author

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,20 +1,49 @@
 class StoriesController < ApplicationController
-  def index
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
+
+  def index
+    @stories = Story.all
   end
 
   def show
+    #before_action :set_flat
   end
 
   def new
+    @story = Story.new
   end
 
   def edit
+    #before_action :set_flat
+  end
+
+  def create
+    story = Story.create(story_params)
+
+    redirect_to story_path(story)
   end
 
   def update
+    #before_action :set_flat
+    @story.update!(story_params)
   end
 
-  def delete
+  def destroy
+    #before_action :set_flat
+    @story.destroy
+
+    redirect_to stories_path
   end
+
+  private
+
+  def set_story
+    @story = Story.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :body)
+  end
+
 end
