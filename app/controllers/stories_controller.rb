92ed5f8b75c7415b7e9899a8185f94d4997adc9b
benoitlecorre <bnt.lecorre@gmail.com>
2014-05-26 15:23:36 +0200

    model and controller for stories created

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,20 @@
+class StoriesController < ApplicationController
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
