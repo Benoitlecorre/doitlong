0cfa696976e1e08d50041f038accf27761660ced
benoitlecorre <bnt.lecorre@gmail.com>
2014-05-29 16:40:43 +0200

    Add Figaro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529115425) do
+ActiveRecord::Schema.define(version: 20140529143603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20140529115425) do
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true, using: :btree
   add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true, using: :btree
+
+  create_table "pictures", force: true do |t|
+    t.integer  "story_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "pictures", ["story_id"], name: "index_pictures_on_story_id", using: :btree
 
   create_table "stories", force: true do |t|
     t.string   "title"
